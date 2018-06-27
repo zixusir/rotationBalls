4f5c93d390a2dfaf65162cb1d7c1f0ee8c22d8b8
zixusir <zixu_sir@163.com>
2018-06-27 20:58:09 +0800

    精灵的点击事件还需要修改

--- phaser-es6-webpack/src/states/Over.js
+++ phaser-es6-webpack/src/states/Over.js
@@ -4,25 +4,67 @@ export default class extends Phaser.State {
   constructor () {
     super()
     this.asset = null
+    this.score = 0
+    this.highestScore = 0
   }
   init () {
-    this.text = this.game.score
+    //this.score = this.game.score
   }
 
   preload () { }
 
   create () {
-    this.text = new Phaser.Graphics(this.game, 0, 0)
-    this.text.beginFill(0xffee00)
-    this.text.drawRoundedRect(0, 0, 200, 75, 5)
-    this.text.endFill()
+    this.text1 = new Phaser.Text(game, this.game.width / 2, 300, 'GAME OVER', {
+      font: '80px',
+      fill: '#ffee00',
+      align: 'center'
+    })
+    this.text1.anchor.setTo(0.5)
+    
+    this.uScore = new Phaser.Text(game, this.game.width / 2, 400, `Your Score: ${this.score}`, {
+      font: '40px',
+      fill: '#ffee00',
+      align: 'center'
+    })
+    this.uScore.anchor.setTo(0.5)
+    
+    this.hScore = new Phaser.Text(game, this.game.width / 2, 500, `Highest Score: ${this.highestScore}`, {
+      font: '40px',
+      fill: '#ffee00',
+      align: 'center'
+    })
+    this.hScore.anchor.setTo(0.5)
+    
+    this.add.existing(this.uScore)
+    this.add.existing(this.hScore)
+    this.add.existing(this.text1)
+    
+    this.text2 = new Phaser.Graphics(this.game, 0, 0)
+    this.text2.beginFill(0xffee00)
+    this.text2.drawRoundedRect(0, 0, 200, 75, 5)
+    this.text2.endFill()
 
-    this.textTexture  = this.text.generateTexture()
-    this.textSprite = new Phaser.Sprite(game, this.game.width / 2, this.game.height / 2, this.textTexture)
-    this.add.existing(this.textSprite)
-
+    this.textTexture  = this.text2.generateTexture()
+    this.textSprite = new Phaser.Sprite(game, this.game.width / 2, this.game.height / 2 + 100, this.textTexture)
+    
     this.textSprite.anchor.setTo(0.5)
-
+    
+    this.text3 = new Phaser.Text(game, 0, 0, 'Try Again', {
+      font: '40px',
+      fill: '#ee5000',
+      align: 'center'
+    })
+    this.text3.anchor.setTo(0.5)
+    this.textSprite.addChild(this.text3)
+    this.add.existing(this.textSprite)
+    
+    this.textSprite.inputEnabled = true
+    
+    this.textSprite.input.onTap.add(tapHandle, this)
+   
     // this.add.existing(this.text)
+  }
+  tapHandle () {
+    console.log('tap事件触发')
   }
 }
